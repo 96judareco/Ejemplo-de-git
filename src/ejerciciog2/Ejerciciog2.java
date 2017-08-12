7b7022b82aaa3c1cf39781043a41aaaeace57983
96judareco <delben555@gmail.com>
2017-08-12 12:22:53 -0500

    Update Ejerciciog2.java

--- src/ejerciciog2/Ejerciciog2.java
+++ src/ejerciciog2/Ejerciciog2.java
@@ -16,7 +16,7 @@ public class Ejerciciog2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hola");
+        System.out.print("Hola Mundo pez");
         
     }
     
